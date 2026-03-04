42f4e3ad0de969e234d0f7cbd25b816453e079f9
Zin Ko Thet <zinkothet153586@gmail.com>
2026-03-04 22:42:44 +0630

    Fianlized A

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
-    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('reset/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
 ]
